97f51ea89276d9e14ca29398e4634216fd9144f0
anangyoga <aharwinto@gmail.com>
2023-10-11 14:29:16 +0700

    feat: handle limit Title chars

--- src/components/NotesForm.jsx
+++ src/components/NotesForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import NotesSection from "./NotesSection";
@@ -5,9 +6,17 @@ import NotesSection from "./NotesSection";
 const NotesForm = ({ createItem, items }) => {
   const [title, setTitle] = useState("");
   const [note, setNote] = useState("");
+  const [char, setChar] = useState(50);
+
+  const limitTitleCharacters = (e) => {
+    if (char - title.length === 0) return;
+    setTitle(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (char - title.length < 0) return;
 
     const newNote = {
       id: +new Date(),
@@ -16,6 +25,7 @@ const NotesForm = ({ createItem, items }) => {
       archived: false,
       createdAt: Date.now(),
     };
+
     createItem(newNote);
     setTitle("");
     setNote("");
@@ -26,8 +36,8 @@ const NotesForm = ({ createItem, items }) => {
         <h2>create your nyotes</h2>
         <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
           <div className="flex flex-col">
-            <span className="text-right">50 characters left</span>
-            <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="title..." className="border border-green-400 bg-black p-2 rounded-md outline-none" />
+            <span className={char - title.length < 10 ? `text-red-500 text-right` : `text-right`}>{char - title.length} characters left</span>
+            <input value={title} onChange={limitTitleCharacters} type="text" placeholder="title..." className="border border-green-400 bg-black p-2 rounded-md outline-none" />
           </div>
           <textarea value={note} onChange={(e) => setNote(e.target.value)} type="text" placeholder="write your notes.." className="border border-green-400 bg-black min-h-[100px] p-2 rounded-md outline-none" />
           <button type="submit" className="bg-green-400 rounded-md text-black font-bold lowercase hover:bg-green-500">
