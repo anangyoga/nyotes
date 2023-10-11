273b7ef013a0e2816b5c98107b8fe4722df68c9e
anangyoga <aharwinto@gmail.com>
2023-10-11 13:23:45 +0700

    fix: search radius

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <nav className="flex justify-between items-center py-5">
       <h1 className="text-2xl font-bold text-green-400">Nyotes</h1>
-      <input type="text" placeholder="Search your notes ..." className="px-5 py-1 bg-black outline-none border border-green-400 text-green-50" />
+      <input type="text" placeholder="Search your notes ..." className="px-5 py-1 bg-black outline-none border border-green-400 text-green-50 rounded-md" />
     </nav>
   );
 };
