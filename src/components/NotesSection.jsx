9f247cd9d72e8760913930e6a528b116d7ada6fc
anangyoga <aharwinto@gmail.com>
2023-10-12 11:38:03 +0700

    fix: added Search functionality to archive

--- src/components/NotesSection.jsx
+++ src/components/NotesSection.jsx
@@ -6,7 +6,7 @@ const NotesSection = ({ items, deleteItem, search, archiveItem }) => {
     <div className="pb-20 md:px-28 lg:px-0 md:pb-32 flex flex-col gap-10">
       <div className="flex flex-col gap-3">
         <h2>Active</h2>
-        <div className="flex flex-col gap-5 md:gap-2 md:flex-wrap">
+        <div className="flex flex-col md:flex-row gap-5 md:gap-2 md:flex-wrap">
           {items.filter((item) => !item.isArchived).length === 0 && <p className="text-slate-500">no nyotes. please make one.</p>}
           {items
             .filter((item) => {
@@ -17,9 +17,13 @@ const NotesSection = ({ items, deleteItem, search, archiveItem }) => {
       </div>
       <div className="flex flex-col gap-3">
         <h2>Archive</h2>
-        <div className="flex flex-col gap-5 md:gap-2 md:flex-wrap">
+        <div className="flex flex-col md:flex-row gap-5 md:gap-2 md:flex-wrap">
           {items.filter((item) => item.isArchived).length === 0 && <p className="text-slate-500">no archived.</p>}
-          {items.map((item) => (item.isArchived ? <NotesCard deleteItem={deleteItem} key={item.id} item={item} archiveItem={archiveItem} /> : null))}
+          {items
+            .filter((item) => {
+              return search.toLowerCase() === item ? "" : item.title.toLowerCase().includes(search);
+            })
+            .map((item) => (item.isArchived ? <NotesCard deleteItem={deleteItem} key={item.id} item={item} archiveItem={archiveItem} /> : null))}
         </div>
       </div>
     </div>
